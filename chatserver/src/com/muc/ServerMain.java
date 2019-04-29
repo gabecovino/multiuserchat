27109d5839deafc031018c92fd8f9f5c58876e85
gabecovino <vcovi6245@westada.org>
2019-04-29 12:19:55 -0600

    Time and date print out

---
+++ chatserver/src/com/muc/ServerMain.java
@@ -0,0 +1,30 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                OutputStream outputStream = clientSocket.getOutputStream();
+                for (int i=0; i<10; i++) {
+                    outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+                    Thread.sleep(200);
+                }
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
