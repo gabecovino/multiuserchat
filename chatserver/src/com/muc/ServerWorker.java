5b96772b254d895563045288d087337f45a1d2f4
gabecovino <vcovi6245@westada.org>
2019-05-07 12:46:31 -0600

    Multiple servers can login and logout to different accounts and every other terminal receives a message saying who logged in or out

--- chatserver/src/com/muc/ServerWorker.java
+++ chatserver/src/com/muc/ServerWorker.java
@@ -4,17 +4,19 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Date;
-
-
+import java.util.List;
 
 
 public class ServerWorker extends Thread{
 
     private final Socket clientSocket;
+    private String login = null;
+    private final Server server;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket) {
+    public ServerWorker(Server server, Socket clientSocket) {
         this.clientSocket = clientSocket;
+        this.server = server;
     }
 
     @Override
@@ -35,7 +37,7 @@ public class ServerWorker extends Thread{
 
     private void handleClientSocket(Socket clientSocket) throws InterruptedException, IOException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -43,7 +45,8 @@ public class ServerWorker extends Thread{
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0){
                 String cmd = tokens[0];
-                if ("quit".equalsIgnoreCase(cmd)) {
+                if ( "logout".equalsIgnoreCase(cmd) || "quit".equalsIgnoreCase(cmd)) {
+                    handleLogOff();
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
@@ -51,14 +54,66 @@ public class ServerWorker extends Thread{
                     String msg = "unknown: " + cmd + "\n";
                     outputStream.write(msg.getBytes());
                 }
-                String msg = "You typed: " + line + "\n";
-                outputStream.write(msg.getBytes());
             }
         }
 
         clientSocket.close();
     }
 
-    private void handleLogin(OutputStream outputStream, String[] tokens) {
+    private void handleLogOff() throws IOException {
+        List<ServerWorker> workerList = server.getWorkerList();
+
+        String onlineMsg = "logout: " + login + "\n";
+        for(ServerWorker worker : workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+        clientSocket.close();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if ((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("jim")) ) {
+                String msg = "ok login\n";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User logged in successfully: " + login);
+                List<ServerWorker> workerList = server.getWorkerList();
+                for(ServerWorker worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "online " + login + "\n";
+                            send(msg2);
+                        }
+                    }
+                }
+
+                String onlineMsg = "online " + login + "\n";
+                for(ServerWorker worker : workerList) {
+                     if (!login.equals(worker.getLogin())) {
+                         worker.send(onlineMsg);
+                     }
+                }
+
+            } else if (login.equals("guest") || login.equals("jim")) {
+                String msg = "wrong password\n";
+                outputStream.write(msg.getBytes());
+            } else {
+                String msg = "error login\n";
+                outputStream.write(msg.getBytes());
+            }
+        }
+    }
+
+    private void send(String onlineMsg) throws IOException {
+        outputStream.write(onlineMsg.getBytes());
     }
 }
